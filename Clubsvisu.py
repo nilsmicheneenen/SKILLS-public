3665efa3093d2ca46639b27e39b57b7c1dd443c6
nilsmicheneenen <nilsmichnguyen@gmail.com>
2025-05-28 09:01:09 +0200

    Update Clubsvisu.py

--- Clubsvisu.py
+++ Clubsvisu.py
@@ -82,12 +82,12 @@ def main():
         # create a cross-tab (pivot table) to count recruitment status per member size category
         ct = pd.crosstab(df['orderedMembers'], df['isRecruiting'])
 
+        plt.style.use('bmh')# Apply a style for homogeneity
         fig4, ax4 = plt.subplots(figsize=(6, 6))
-        ct.plot(kind='bar', ax=ax4)  # Bar chart
+        ct.plot(kind='bar',ax=ax4,color={'No': 'red', 'Yes': 'blue'}  # Map 'No' → red, 'Yes' → blue) #select specific color in order to have consitency with row 1 graphs
         ax4.set_xlabel("Number of Members")#X-axis label
         ax4.set_ylabel("Number of Clubs")  #Y-axis label
         ax4.set_title("Member Size vs. Recruitment Status")
-        plt.style.use('bmh')# Apply a style for homogeneity
         ax4.legend(title="Recruiting ?") # adding a legend for clarity of the graph
         st.pyplot(fig4)
 
